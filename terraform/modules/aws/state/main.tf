335dca47342320175020d918bf644f39fe19bfc8
Ian Cornett <icornett@redapt.com>
2018-04-30 17:12:34 -0700

    update state file after testing, remove provider variables and block.

--- terraform/modules/aws/state/main.tf
+++ terraform/modules/aws/state/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY}"
-  secret_key = "${var.AWS_SECRET_KEY}"
-  region     = "${var.AWS_REGION}"
-}
-
 resource "aws_dynamodb_table" "state-lock-table" {
   name           = "${var.project_name}-lock"
   read_capacity  = 20
--- terraform/modules/aws/state/vars.tf
+++ terraform/modules/aws/state/vars.tf
@@ -1,19 +1,3 @@
-variable "AWS_ACCESS_KEY" {
-  type        = "string"
-  description = "Access credential for AWS"
-}
-
-variable "AWS_SECRET_KEY" {
-  type        = "string"
-  description = "Secret credential for AWS"
-}
-
-variable "AWS_REGION" {
-  type        = "string"
-  default     = "us-west-2"
-  description = "Region to deploy assets to, defaults to US West 2 (Oregon)"
-}
-
 variable "project_name" {
   type        = "string"
   description = "The name of the project being run"
